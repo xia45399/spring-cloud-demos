84140b0d37dc920c8e177d2cf8453726d9bef5a5
xia45399 <453993887@qq.com>
2019-07-02 22:28:36 +0800

    手动刷新配置

--- spring-cloud-config-client-demo/src/main/java/com/summer/springboot/configclient/demo/controller/TestController.java
+++ spring-cloud-config-client-demo/src/main/java/com/summer/springboot/configclient/demo/controller/TestController.java
@@ -1,16 +1,18 @@
 package com.summer.springboot.configclient.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestController {
-    @Value("${fileName}")
-    private String fileName;
+    @Value("${desc}")
+    private String desc;
 
-    @RequestMapping("/getFileName")
-    public String getFileName() {
-        return fileName;
+    @RequestMapping("/getDesc")
+    public String getDesc() {
+        return desc;
     }
 }
